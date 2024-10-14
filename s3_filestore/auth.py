7d7e11cd7ceaf75d73ddd5e3e06642b146c030a2
grez72 <grez72@gmail.com>
2024-10-14 07:10:07 -0400

    Update auth.py

--- s3_filestore/auth.py
+++ s3_filestore/auth.py
@@ -15,8 +15,8 @@ def get_credentials(profile_name):
     # if no profile_name, check environ for credentials
     if profile_name is None:
         return dict(
-            S3_ACCESS_KEY=os.environ['S3_ACCESS_KEY'],
-            S3_SECRET_KEY=os.environ['S3_SECRET_KEY'],
+            S3_ACCESS_KEY_ID=os.environ['S3_ACCESS_KEY_ID'],
+            S3_SECRET_ACCESS_KEY=os.environ['S3_SECRET_ACCESS_KEY'],
             S3_REGION=os.environ['S3_REGION'],
             S3_ENDPOINT_URL=os.environ['S3_ENDPOINT_URL']
         )
@@ -30,8 +30,8 @@ def get_credentials(profile_name):
     endpoint_url = config.get('s3api', {}).get('endpoint_url', WASABI_ENDPOINT if 'wasabi' in profile_name else AWS_ENDPOINT)
 
     return dict(
-        S3_ACCESS_KEY=current_credentials.access_key,
-        S3_SECRET_KEY=current_credentials.secret_key,
+        S3_ACCESS_KEY_ID=current_credentials.access_key,
+        S3_SECRET_ACCESS_KEY=current_credentials.secret_key,
         S3_REGION=region,
         S3_ENDPOINT_URL=endpoint_url
     )
@@ -46,8 +46,8 @@ def get_userdata(profile='wasabi'):
 
 def get_session_with_userdata(profile, region_name=None):
     userdata = get_userdata(profile=profile)
-    session = boto3.Session(aws_access_key_id=userdata.get('S3_ACCESS_KEY'),
-                            aws_secret_access_key=userdata.get('S3_SECRET_KEY'),
+    session = boto3.Session(aws_access_key_id=userdata.get('S3_ACCESS_KEY_ID'),
+                            aws_secret_access_key=userdata.get('S3_SECRET_ACCESS_KEY'),
                             region_name=region_name)
     return session
 
@@ -65,8 +65,8 @@ def get_bucket_location(bucket_name, profile='wasabi'):
 def get_client_with_userdata(profile='wasabi'):
     userdata = get_userdata(profile=profile)
     s3_client = boto3.client('s3', 
-                             aws_access_key_id=userdata.get('S3_ACCESS_KEY'),
-                             aws_secret_access_key=userdata.get('S3_SECRET_KEY'),
+                             aws_access_key_id=userdata.get('S3_ACCESS_KEY_ID'),
+                             aws_secret_access_key=userdata.get('S3_SECRET_ACCESS_KEY'),
                              endpoint_url=userdata.get('S3_ENDPOINT_URL'))
 
     return s3_client
